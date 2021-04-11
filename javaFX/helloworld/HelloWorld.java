07cf2ca44d97d3aa39eddbf26ee1dbf66b187c81
Jessica <77985155+Jessica-RS@users.noreply.github.com>
2021-04-11 00:19:10 -0600

    Experimenting with lists, adding icons
    Added two new sample programs ListViewSample.java and ButtonList.java to demonstrate lists as an option for formatting our application.
    Also figured out the code for adding icons.

--- javaFX/helloworld/HelloWorld.java
+++ javaFX/helloworld/HelloWorld.java
@@ -1,36 +1,60 @@
 package helloworld;
+
  
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import java.io.*;
- 
+
+
 public class HelloWorld extends Application {
     public static void main(String[] args) {
         launch(args);
     }
     
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception {
+
         primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        //btn.setMinWidth(100);
-        btn.setMaxWidth(200);
-        btn.setMaxHeight(50);
         
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+        FileInputStream input = new FileInputStream("helloworld/icons/canvas.png");
+        Image image = new Image(input);
+        ImageView imageView = new ImageView(image);
+        imageView.setFitWidth(16);
+        imageView.setFitHeight(16);
+        Button btn1 = new Button("Canvas", imageView);
+        //btn.setMinWidth(100);
+        btn1.setMaxWidth(200);
+        btn1.setMaxHeight(50);
+        
+        /*btn1.setText();
+        Image image
+        
+        Button btn2 = new Button();
+        btn2.setMaxWidth(200);
+        btn2.setMaxHeight(50);
+        btn2.setText("button2");
+        //btn2.setIcon(new ImageIcon("C:\Users\stein.000\Documents\GitHub\workspace\NotificationCentral\javaFX\helloworld\icons\canvas.png"));
+        Image canvas = new Image(getClass().getResourcesAsStream("canvas.png"));
+        btn2.setGraphic(new ImageView(canvas));
+        */
+        btn1.setOnAction(new EventHandler<ActionEvent>() {
  
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("Hello World!");
-                btn.setMaxHeight(200);
-                btn.setMaxWidth(200);
-                btn.setText("Okay, I get it....");
+                btn1.setMaxHeight(200);
+                btn1.setMaxWidth(200);
+                btn1.setText("Okay, I get it....");
+                primaryStage.setMaxWidth(200);
                 
                 try {
 			         //System.out.println("Opening notepad");
@@ -70,8 +94,12 @@ public class HelloWorld extends Application {
         });
         
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        root.getChildren().add(btn1);
         primaryStage.setScene(new Scene(root, 300, 250));
         primaryStage.show();
+        //when you maximize the window, it sticks to the left of the screen
+        primaryStage.setHeight(500);
+        primaryStage.setWidth(100);
+        primaryStage.setMaxWidth(200);
     }
 }
